978aa1c58a259e31a983192f7b9bb559bf1ccf91
Erik Thiem <ethiem@covermymeds.com>
2018-02-02 18:06:30 -0500

    Added video categories with some constraints and seeds

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -2,6 +2,9 @@ defmodule Rumbl.VideoController do
   use Rumbl.Web, :controller
 
   alias Rumbl.Video
+  alias Rumbl.Category
+
+  plug :load_categories when action in [:new, :create, :edit, :update]
 
   def index(conn, _params, user) do
     videos = Repo.all(user_videos(user))
@@ -77,5 +80,14 @@ defmodule Rumbl.VideoController do
 
   defp user_videos(user) do
     assoc(user, :videos)
+  end
+
+  defp load_categories(conn, _) do
+    query =
+      Category
+      |> Category.alphabetical
+      |> Category.names_and_ids
+    categories = Repo.all query
+    assign(conn, :categories, categories)
   end
 end
--- web/models/video.ex
+++ web/models/video.ex
@@ -6,16 +6,20 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
 
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> assoc_constraint(:category)
   end
 end
