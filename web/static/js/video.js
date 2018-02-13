4cb7f5ec51e54e8edd0f281ba17226dc2eea62ea
Erik Thiem <ethiem@covermymeds.com>
2018-02-13 12:17:44 -0500

    annotations galore

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -21,16 +21,30 @@ let Video = {
       let payload = {body: msgInput.value, at: Player.getCurrentTime()}
       vidChannel.push("new_annotation", payload)
         .receive("error", e => console.log(e))
-        msgInput.value = ""
+      msgInput.value = ""
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
 
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+        e.target.parentNode.getAttribute("data-seek")
+      if (!seconds){ return }
+
+      Player.seekTo(seconds)
+    })
+
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
-      .receive("error", reason => console.log("join failed", reason))
+      .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if (ids.length > 0){ vidChannel.params.last_seen_id = Math.max(...ids) }
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
+      .receive("error", reason => console.log("join failed", reason) )
     },
 
     esc(str){
@@ -44,11 +58,37 @@ let Video = {
 
       template.innerHTML = `
       <a href="#" data-seek="${this.esc(at)}">
+        [${this.formatTime(at)}]
         <b>${this.esc(user.username)}</b>: ${this.esc(body)}
       </a>
       `
       msgContainer.appendChild(template)
       msgContainer.scrollTop = msgContainer.scrollHeight
+    },
+
+    scheduleMessages(msgContainer, annotations){
+      setTimeout(() => {
+        let ctime = Player.getCurrentTime()
+        let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+        this.scheduleMessages(msgContainer, remaining)
+      }, 1000)
+    },
+
+    renderAtTime(annotations, seconds, msgContainer){
+      return annotations.filter( ann => {
+        if(ann.at > seconds){
+          return true
+        } else {
+          this.renderAnnotation(msgContainer, ann)
+          return false
+        }
+      })
+    },
+
+    formatTime(at){
+      let date = new Date(null)
+      date.setSeconds(at / 1000)
+      return date.toISOString().substr(14, 5)
     }
 }
 
